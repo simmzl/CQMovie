ec5d6b2e22d2eec08fa0c8c85194323c42bc5cec
simm <czl008@outlook.com>
2017-07-21 22:23:52 +0800

    0721

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 /**
  * Created by zelong on 2017/7/19.
  */
-$(document).ready(function(){
-
+//$(document).ready(function(){
+//    $(.a-color)
+//});
+//document.onload.alert("hahaha");
+//alert(7);
+//<script>
+$(function() {
+    $('body,html').animate({scrollTop:$(window).height()}, 500);
 });
+//</script>
