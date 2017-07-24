098adb23a1dc7e9447d2c2fabead38205d8cc606
simm <czl008@outlook.com>
2017-07-24 14:25:28 +0800

    07241425

--- js/main.js
+++ js/main.js
@@ -1,13 +1,49 @@
-/**
- * Created by zelong on 2017/7/19.
- */
-//$(document).ready(function(){
-//    $(.a-color)
-//});
-//document.onload.alert("hahaha");
-//alert(7);
-//<script>
-$(function() {
-    $('body,html').animate({scrollTop:$(window).height()}, 500);
+///**
+// * Created by zelong on 2017/7/19.
+// */
+//        有点笨拙
+function mTurn ( obj ){
+    var mColor = obj.css("color");
+//                alert(mColor);
+    $('#section1').css('background',mColor);
+}
+function mTurnOut(obj){
+    $('#section1').css('background','#ffffff');
+}
+
+$(document).ready(function(){
+    $('#drama').mouseover(function(){
+        mTurn($('#drama'));
+    });
+    $('#drama').mouseout(function(){
+        mTurnOut($('#drama'));
+    });
+
+    $('#war').mouseover(function(){
+        mTurn($('#war'));
+    });
+    $('#war').mouseout(function(){
+        mTurnOut($('#war'));
+    });
+
+    $('#romance').mouseover(function(){
+        mTurn($('#romance'));
+    });
+    $('#romance').mouseout(function(){
+        mTurnOut($('#romance'));
+    });
+
+    $('#sci').mouseover(function(){
+        mTurn($('#sci'));
+    });
+    $('#sci').mouseout(function(){
+        mTurnOut($('#sci'));
+    });
+
+    $('#anim').mouseover(function(){
+        mTurn($('#anim'));
+    });
+    $('#anim').mouseout(function(){
+        mTurnOut($('#anim'));
+    });
 });
-//</script>
